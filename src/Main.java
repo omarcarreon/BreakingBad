647a68c1fcef588e6e8dcaab086043d0f3463bed
Omar <oomarcarreon@gmail.com>
2014-09-30 14:14:43 -0500

    Merge origin/master
    Conflicts:
    	src/AppletExamen.java

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+
+
+import javax.swing.JFrame;
+import java.io.IOException;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Omar Antonio Carreón Medrano A01036074
+ * @author Gabriel Salazar De Urquidi A01139126
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        JFrameBreakingBad jfrJFrameBreakingBad = new JFrameBreakingBad();
+        jfrJFrameBreakingBad.setVisible(true);
+        jfrJFrameBreakingBad.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    
+}
